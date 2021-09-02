6dd95055260c7fd34acf178c85d4478404544b16
Kickjaw <jaredgreeneperkins@gmail.com>
2021-09-02 16:27:36 -0400

    update
    met with client on needs, documented in readme, tried to add in edge collision removal for tiling

--- Readme.txt
+++ Readme.txt
@@ -5,7 +5,7 @@ objects -> oObject
 
 
 
-
+https://www.pyimagesearch.com/2016/03/28/measuring-size-of-objects-in-an-image-with-opencv/
 
 need to tile input images
 
@@ -19,6 +19,8 @@ https://www.youtube.com/watch?v=FjyF03uawsA
 deep nueral networks in opencv
 
 https://docs.opencv.org/3.4/d6/d0f/group__dnn.html#ga138439da76f26266fdefec9723f6c5cd
+https://learnopencv.com/deep-learning-with-opencvs-dnn-module-a-definitive-guide/#what-is-opencvv-dnn-module
+https://www.pyimagesearch.com/2018/11/12/yolo-object-detection-with-opencv/
 
 
 
@@ -29,3 +31,43 @@ exception handling
 edge case
 error logging
 error reporting to email system
+
+
+
+Notes:
+
+
+V1
+
+end user open the image, click button, identify mussels, take measurements, output image and table to files
+
+different color bounding boxes per class
+
+options for end user:
+batch processing
+toggle detection boxes
+- toggle by mussel type
+input pixel to inch scaling factor
+
+
+
+measurements: 
+length, width, hinge, aligned to muscle
+
+error log
+
+output table: importable into spreadsheet, common format, csv or such
+flag errors
+
+table id, mussel ID, length, width, hinge, 
+
+build as .exe or universal exacutable
+
+
+
+
+V2
+interactable bounding boxs that effect table
+alternative setups
+alternative resolutions
+automatic sclaing factor input with known dimensional target
--- detection.py
+++ detection.py
@@ -13,35 +13,37 @@ class darkentDetection(object):
     def __init__(self):
         self.image = cv2.imread('3.jpg')
         self.tilesize = 512
-        self.overlap = 40
+        self.overlap = 10
         self.classes = ['mussel', 'mussel_open']
         self.net = cv2.dnn.readNetFromDarknet('yolov4-obj-tilled.cfg', 'yolov4-obj-tilled_best.weights')
         self.model = cv2.dnn_DetectionModel(self.net)
         self.model.setInputParams(scale=1 / 255, size=(512, 512), swapRB=True)
         self.tiles = [] #list of tiles, tile = [tilledImage, tileYcord, tileXcord]
+        self.tilledDimensions = [0,0] #[numOfXtiles, numOfYtiles]
         self.detections = [] #list of detected items, = []
 
 
     #takes in an image and overlap and returns a list of images
     #returnes list of images all the same size, if the cut tile is to small pad with black pixels
     def mTileImage(self):
-        x_numCopies = int(np.ceil(self.image.shape[1]/(self.tilesize-(self.overlap*2))))-1
-        y_numCopies = int(np.ceil(self.image.shape[0]/(self.tilesize-(self.overlap*2))))-1
+        self.tilledDimensions[0] = int(np.floor(self.image.shape[1]/(self.tilesize-self.overlap)))
+        self.tilledDimensions[1] = int(np.floor(self.image.shape[0]/(self.tilesize-self.overlap)))
+        print(self.tilledDimensions)
 
-        for i in range(y_numCopies):
-            for j in range(x_numCopies):
+        for i in range(self.tilledDimensions[1]):
+            for j in range(self.tilledDimensions[0]):
                 #imagecrop[startheight:endheight, startwidth:endwidth]
                 #if the top left corner
                 if j == 0 and i == 0:
-                    tempImage = self.image[0:(self.tilesize)*(i+1),0:(self.tilesize)*(j+1)]
+                    tempImage = self.image[0:self.tilesize,0:self.tilesize]
                 #if left most image
                 elif j ==0:
-                    tempImage = self.image[(self.tilesize)*i-self.overlap:(self.tilesize)*(i+1)-self.overlap,0:(self.tilesize)*(j+1)]
+                    tempImage = self.image[(self.tilesize)*i-self.overlap:(self.tilesize)*(i+1)-self.overlap,0:self.tilesize]
                 #if in top row
                 elif i ==0:
-                    tempImage = self.image[0:(self.tilesize)*(i+1),(self.tilesize)*j-self.overlap:(self.tilesize)*(j+1)-self.overlap]
+                    tempImage = self.image[0:self.tilesize,(self.tilesize)*j-self.overlap:(self.tilesize)*(j+1)-self.overlap]
                 else:
-                    tempImage = self.image[(self.tilesize)*i-self.overlap:(self.tilesize)*(i+1)-self.overlap,(self.tilesize)*j-self.overlap:(self.tilesize)*(j+1)-self.overlap]
+                    tempImage = self.image[(self.tilesize*i)-self.overlap:(self.tilesize)*(i+1)-self.overlap,(self.tilesize*j)-self.overlap:(self.tilesize)*(j+1)-self.overlap]
 
 
                 #pad the image with blackness if it not large enough to be tile sized
@@ -53,48 +55,116 @@ class darkentDetection(object):
 
     #takes all the tiles and returns all the bounding boxes
     def mDetectMussels(self):
+        print('detecting')
         #run the detection
         for tile in self.tiles:
-            classIds, scores, boxes = self.model.detect(tile[0], confThreshold=0.6, nmsThreshold=0.4)
+            #maybe run the detection with out any of the non max suppresion, then combine all the boxes, then run the nonmax supression
+            classIds, scores, boxes = self.model.detect(tile[0], confThreshold=0.6, nmsThreshold=0.5)
             #box = [xcord, ycord, boxwidth, boxheight]
             #add in for loop to go over boxes and append them to the main box list, while translating them in
+
+
             #space so that they line up with the original
             for (classId, score, box) in zip(classIds, scores, boxes):
-                if tile[1] == 0 and tile[2] == 0:
-                    boxTranslated = box
-                elif tile[2] == 0:
-                    boxTranslated = [box[0], box[1]+(tile[1]*512-self.overlap), box[2], box[3]]
-                elif tile[1] == 0:
-                    boxTranslated = [box[0]+(tile[2]*512-self.overlap), box[1], box[2], box[3]]
-                else:
-                    boxTranslated = [box[0]+(tile[2]*512-self.overlap), box[1]+(tile[1]*512-self.overlap), box[2], box[3]]
-                self.detections.append([classId, score, boxTranslated])
+                # collisionsToUse = self.mEdgesToUse(tile)
+                # if self.mEdgeCollision(box, collisionsToUse):
+                    if tile[1] == 0 and tile[2] == 0:
+                        boxTranslated = box
+                    elif tile[2] == 0:
+                        boxTranslated = [box[0], box[1]+(tile[1]*512-self.overlap), box[2], box[3]]
+                    elif tile[1] == 0:
+                        boxTranslated = [box[0]+(tile[2]*512-self.overlap), box[1], box[2], box[3]]
+                    else:
+                        boxTranslated = [box[0]+(tile[2]*512-self.overlap), box[1]+(tile[1]*512-self.overlap), box[2], box[3]]
+                    #change this it numpy array
+                    
+                    self.detections.append([classId, score, boxTranslated])
 
-            #     cv2.rectangle(tile[0], (box[0], box[1]), (box[0] + box[2], box[1] + box[3]),
-            #                 color=(0, 255, 0), thickness=2)
-            
-            #     text = '%s: %.2f' % (self.classes[classId[0]], score)
-            #     cv2.putText(tile[0], text, (box[0], box[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 1,
-            #                 color=(0, 255, 0), thickness=2)
-            
-            # cv2.imshow('Image', tile[0])
-            # cv2.waitKey(0)
-            # cv2.destroyAllWindows()
+    #method to determine if dectection should be used or not with overlap
+    #returns bool
+    def mEdgeCollision(self, box, collisionstouse):
+        #tile = [tilledImage, tileYcord, tileXcord]
 
+        collisionLTRB = [False, False, False, False] #list of bools indicating if box is colliding with left, top, right, bottom edges of the tile
+        
+        #check which edges the box collides with
+        #box = [xcord, ycord, boxwidth, boxheight]
+        if box[0] < 2:
+            collisionLTRB[0] = True
+        if box[1] < 2:
+            collisionLTRB[1] = True
+        if box[0] + box[2] > self.tilesize-2:
+            collisionLTRB[2] = True
+        if box[1] + box[3] > self.tilesize-2:
+            collisionLTRB[3] = True
+        if any(collisionLTRB):
+            if collisionLTRB == collisionstouse:
+                return True
+            else:
+                return False
+        else:
+            return True
+
+    #method to check which detections to use based on tile location
+    #returns a list of 4 bools, left, top, right, bottom, of wether to use that edge or not
+    def mEdgesToUse(self, tile):
+        #check which edges the current tile will use
+        #if in the top left corner use left and top edge detections
+        if tile[1] == 0 and tile[2] == 0:
+            collisionLTRBtoUse = [True, True, False, False]
+        #if in the top row use top edge detections
+        elif tile[1] == 0:
+            collisionLTRBtoUse = [False, True, False, False]
+        #if in the left colum use left edge detections
+        elif tile[2] == 0:
+            collisionLTRBtoUse = [True, False, False, False]
+        #if in the right column use right edge detections
+        elif tile[2] == self.tilledDimensions[0]:
+            collisionLTRBtoUse = [False, False, True, False]
+        #in in the bottom row use bottom edge detections
+        if tile[1] == self.tilledDimensions[1]:
+            collisionLTRBtoUse = [False, False, False, True]
+        #if in the bottom right corner use bottom and right edge detections
+        elif tile[2] == self.tilledDimensions[0] and tile[1] == self.tilledDimensions[1]:
+            collisionLTRBtoUse = [False, False, True, True]
+        #else use left and top detections
+        else:
+            collisionLTRBtoUse = [False, False, False, False]
+
+        return collisionLTRBtoUse
+
+
+    #method to remove overlaping bounding boxes in the overlaping sections of the tiles
+    def mMergeOverlaping(self):
+        print('merging)')
+        
+
+        pass
+
+ 
+    #add in functionality to display tile lines
     def mDisplayDetections(self):
+        print('display')
+        musselCount = 0
+        open_musselCount = 0
         for detection in self.detections:
             classId = detection[0]
             score = detection[1]
             box = detection[2]
-            cv2.rectangle(self.image, (box[0], box[1]), (box[0] + box[2], box[1] + box[3]),
-                            color=(0, 255, 0), thickness=2)
+            if classId == 0:
+                musselCount +=1
+                cv2.rectangle(self.image, (box[0], box[1]), (box[0] + box[2], box[1] + box[3]), color=(255, 255, 255), thickness=1)
+            elif classId == 1:
+                open_musselCount +=1
+                cv2.rectangle(self.image, (box[0], box[1]), (box[0] + box[2], box[1] + box[3]), color=(0, 255, 0), thickness=1)
             
-            text = '%s: %.2f' % (self.classes[classId[0]], score)
-            cv2.putText(self.image, text, (box[0], box[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                        color=(0, 255, 0), thickness=2)
+            
+            #text = '%s: %.2f' % (self.classes[classId[0]], score)
+            #cv2.putText(self.image, text, (box[0], box[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 1,color=(0, 255, 0), thickness=2)
         im2 = self.image.copy()
         im2[:, :, 0] = self.image[:, :, 2]
-        im2[:, :, 2] = self.image[:, :, 0] 
+        im2[:, :, 2] = self.image[:, :, 0]
+        print(musselCount, open_musselCount)
         plt.imshow(im2)
         plt.show()
     
